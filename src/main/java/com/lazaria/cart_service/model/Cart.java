9512abea201d6e6749ef438f71dcd8b1b01c9a98
LazariaInk <lorienlored@gmail.com>
2024-08-21 21:26:30 +0300

    add cart and cart items model

---
+++ src/main/java/com/lazaria/cart_service/model/Cart.java
@@ -0,0 +1,29 @@
+package com.lazaria.cart_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "cart_item_id", referencedColumnName = "id")
+    private List<CartItem> items = new ArrayList<>();
+}
